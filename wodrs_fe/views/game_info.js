5cf2613f7ccd8845c5cce51f805765085f4ef4be
lesion <lesion@autistici.org>
2013-05-18 21:41:45 +0200

    yo

--- wodrs_fe/views/game_info.js
+++ wodrs_fe/views/game_info.js
@@ -1,10 +1,14 @@
 
-<h3 class='center'><%=game.player1.username%> VS <%=game.player2.username%></h3>
-<h3 class='center'><%=game.score1%> - <%=game.score2%></h3>
+<h3 class='center'><%=game.player1%> VS <%=game.player2%></h3>
+
+<h3 class='center'>
+  <%if (game.score1==-1){%>X<%}else{%><%=game.score1%><%}%> - 
+  <%if (game.score2==-1){%>X<%}else{%><%=game.score2%><%}%>
+</h3>
 
 <div class='center'>
   <a class='button' href='game_list'>Back</a>
-  <% if(game.state=='running') { %>
+  <% if(game.state=='running' && game.score1==-1) { %>
     <a class='button' href='start_game/<%=game.gamesId%>'>PLAY!</a>
   <% } %>
 </div>
