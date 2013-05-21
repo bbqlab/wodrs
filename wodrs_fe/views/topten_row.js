72e6e55ece773a8b01d1fd103a6a1ecc7ac45190
lesion <lesion@autistici.org>
2013-05-22 01:44:47 +0200

    yo

--- wodrs_fe/views/topten_row.js
+++
@@ -1,6 +0,0 @@
-<% console.log('in topten row'); %>
-<li>
-  <span class='right player_score'>Did <%=score.score%> points!</span>
-  <span class='player_pos'><%=pos%>.</span>
-  <span class='player_name'><%=score.player%></span>
-</li>
